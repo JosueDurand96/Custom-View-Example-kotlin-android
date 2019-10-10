d4683a4f5befc9928b54384f5f36433263322566
JosueDurand96 <josue.durand8@gmail.com>
2019-10-10 15:29:03 -0500

    primer

--- app/src/main/java/com/example/customviewexample/MainActivity.kt
+++ app/src/main/java/com/example/customviewexample/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.customviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = TextView ( this )
+        textView.text = "Hola vistas personalizadas"
+        setContentView (textView)
     }
 }
